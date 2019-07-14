fadebe279af53345216790e220aabc4a9dea5eb4
kemalizing <36174398+kemalizing@users.noreply.github.com>
2019-07-14 13:57:40 +0300

    Update vv.user.js

--- vv.user.js
+++ vv.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vakantieveilingen buy
 // @namespace    http://vakantieveilingen.nl/
-// @version      1.3.2
+// @version      1.3.3
 // @updateURL    https://github.com/kemalizing/tamper-scripts/raw/master/vv.user.js
 // @description  try to take over the world!
 // @author       You
@@ -23,6 +23,7 @@ var maxBidKey = bidName+"_maxBid";
 var winners = ""
 var winnersKey = bidName+"_winners";
 
+var avgWinner = null;
 var minWinner = null;
 var minWinnerKey = bidName+"_minWinner";
 
@@ -34,7 +35,7 @@ function mycode() {
         //console.log("maxBidKey:"+maxBidKey+" jq disptime:"+$("#biddingBlock .display-time-value").textContent);
     //}
     var bid = document.getElementById('jsActiveBidInput');
-    var refresh = document.getElementsByClassName('i-refresh-white')[0];
+    var refreshLinks = $("a[data-track-action='reopen auction']");
     var button = document.getElementById('jsActiveBidButton');
     var minBid = document.getElementsByClassName('fastBidValue')[0];
     var timer = document.getElementsByClassName('timer-countdown-label')[0];
@@ -57,7 +58,7 @@ function mycode() {
             }
         }
     }
-    if(refresh) {
+    if(refreshLinks.length>0) {
         (async () => {
 
             winners = await GM.getValue(winnersKey, winners);
@@ -79,6 +80,16 @@ function mycode() {
     }
 }
 
+function average(elmt){
+    var sum = 0;
+    for( var i = 0; i < elmt.length; i++ ){
+        sum += parseInt( elmt[i], 10 ); //don't forget to add the base
+    }
+
+    var avg = sum/elmt.length;
+    return avg;
+}
+
 function abortTimer() {
   clearInterval(tid);
 }
@@ -98,10 +109,12 @@ function setMaxBid() {
     maxBid = await GM.getValue(maxBidKey, maxBid);
     winners = await GM.getValue(winnersKey, winners);
     minWinner = await GM.getValue(minWinnerKey, minWinner);
+    var winnerArr = winners.split(", ");
+    avgWinner = average(winnerArr).toFixed();
 
     var newHTML = document.createElement ('h1');
     newHTML.innerHTML = '<h1 id="vv_note" style="color:red;position:fixed;top:150px;right:100px;z-index:1111"> <strong>WILL BUY UP UNTIL € <input id="vv_maxBid" size=1 value="'+maxBid+'"/> </strong>'+
-        '<br><small>min won price: '+minWinner+' <br>latest won prices: '+winners+'</small></h1>';
+        '<br><small>Min won price: '+minWinner+' Avg won price:'+avgWinner+'<br>latest won prices: '+winners+'</small></h1>';
     document.body.appendChild (newHTML);
     vv_maxBid = document.getElementById('vv_maxBid');
     vv_maxBid.addEventListener ("input", setMaxBid , false);
