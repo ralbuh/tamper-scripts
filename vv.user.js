018c275283854291c071b39061c184c28a416a0c
kemalizing <36174398+kemalizing@users.noreply.github.com>
2021-12-18 19:02:32 +0100

    Update vv.user.js

--- vv.user.js
+++ vv.user.js
@@ -12,32 +12,23 @@
 // ==/UserScript==
 
 var bidName = null;
-var bidNameView = document.getElementById("lotTitle");
-if(bidNameView){
-    bidName = bidNameView.textContent.trim();
-}
 
 var maxBid = 0;
-var maxBidKey = bidName+"_maxBid";
-
 var winners = ""
-var winnersKey = bidName+"_winners";
+var winnersKey, maxBidKey, minWinnerKey;
+
 
 var avgWinner = null;
 var minWinner = null;
-var minWinnerKey = bidName+"_minWinner";
 
 var vv_maxBid;
 var tid;
 
 function mycode() {
-    //if(bidName){
-        //console.log("maxBidKey:"+maxBidKey+" jq disptime:"+$("#biddingBlock .display-time-value").textContent);
-    //}
+
     var bid = document.getElementById('jsActiveBidInput');
-    var refreshLinks = $("a[data-track-action='reopen auction']");
     var button = document.getElementById('jsActiveBidButton');
-    var minBid = document.getElementsByClassName('fastBidValue')[0];
+    var minBid = document.getElementById('jsMainLotCurrentBid');
     var timer = document.getElementsByClassName('timer-countdown-label')[0];
 
     if(button){
@@ -45,20 +36,33 @@ function mycode() {
     } else {
         $("#vv_note").hide();
     }
+
+
     if(bid && timer){
-        //console.log("maxBid:"+maxBid+" minBid:"+minBid.textContent+(minBid.textContent<=maxBid));
+        var minBidInt = parseInt(minBid.textContent);
+        bid.value = minBidInt+1;
+        console.log("maxBid:"+maxBid+" minBid:"+minBid.textContent+" timer:"+timer.textContent+" button:"+button.innerText);
         //setCookie(location, minBid, 1);
 
         if(timer.textContent == "01" ){
-            var minBidInt = parseInt(minBid.textContent);
-            if(minBidInt<=maxBid){
-                bid.value = minBid.textContent;
+            console.log("1 sec left");
+            minBidInt = parseInt(minBid.textContent);
+            if(maxBid>0 && minBidInt<maxBid){
+                console.log("buying");
+                bid.value = minBidInt+1;;
                 button.click();
                 abortTimer();
+                console.log("bought!");
+            } else {
+                console.log("no bid. maxBid:"+maxBid+" minBid:"+minBidInt);
             }
         }
     }
-    if(refreshLinks.length>0) {
+
+    var refreshLinks = $("a[data-track-action='reopen auction']");
+    var auctionClosing = document.getElementsByClassName('auction-closing')
+
+    if(refreshLinks.length>0 || auctionClosing.length>0) {
         (async () => {
 
             winners = await GM.getValue(winnersKey, winners);
@@ -66,9 +70,13 @@ function mycode() {
             if(winnerArr.length>15){
                 winnerArr = winnerArr.slice(winnerArr.length-15, winnerArr.length);
             }
-            var currentWinner = parseInt(document.getElementById("jsMainLotCurrentBid").textContent);
+            var currentWinner = minBidInt;
+            if(minBid){
+                currentWinner = parseInt(minBid.textContent);
+            }
             winnerArr.push(currentWinner);
             winners = winnerArr.join(", ");
+            console.log("updating winners "+winners+" currentWinner:"+currentWinner+ "winnersKey:"+winnersKey);
             GM.setValue(winnersKey, winners);
             if(currentWinner>0 && (!minWinner || currentWinner<minWinner)){
                 minWinner = currentWinner;
@@ -104,7 +112,14 @@ function setMaxBid() {
 
 (async () => {
     'use strict';
+    while(!bidName){
+        bidName = document.getElementById("lotTitle").textContent.trim();
         console.log("XbidName:"+bidName);
+    }
+
+    winnersKey = bidName+"_winners";
+    maxBidKey = bidName+"_maxBid";
+    minWinnerKey = bidName+"_minWinner";
 
     maxBid = await GM.getValue(maxBidKey, maxBid);
     winners = await GM.getValue(winnersKey, winners);
